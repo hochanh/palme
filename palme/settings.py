1c2d8f2da1bf15fd1d567c132802ea9061d44c7d
manhtai <vo@manhtai.com>
2021-10-01 17:37:30 +0700

    Allow me

--- palme/settings.py
+++ palme/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default_sk)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pal-me.herokuapp.com' ]
 
 
 # Application definition
